bc038accf5f3a81e379697a81edc6a296b5baa71
Meta-c <metamedhat116@gmail.com>
2023-03-16 15:05:08 +0200

    add google fonts

--- bookly/lib/core/utils/styles.dart
+++ bookly/lib/core/utils/styles.dart
@@ -2,5 +2,5 @@ import 'package:flutter/widgets.dart';
 
 abstract class Styles {
   static const titleMedium =
-      TextStyle(fontSize: 21, fontWeight: FontWeight.w600);
+      TextStyle(fontSize: 18, fontWeight: FontWeight.w600);
 }
--- bookly/lib/main.dart
+++ bookly/lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:bookly/constants.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-
+import 'package:google_fonts/google_fonts.dart';
 import 'features/Splash/presentation/views/splash_view.dart';
 
 void main() {
@@ -17,8 +17,10 @@ class BooklyApp extends StatelessWidget {
     return GetMaterialApp(
         useInheritedMediaQuery: true,
         debugShowCheckedModeBanner: false,
-        theme: ThemeData.dark()
-            .copyWith(scaffoldBackgroundColor: kBackgroundColor),
+        theme: ThemeData.dark().copyWith(
+            scaffoldBackgroundColor: kBackgroundColor,
+            textTheme:
+                GoogleFonts.montserratTextTheme(ThemeData.dark().textTheme)),
         home: const SplashView());
   }
 }
