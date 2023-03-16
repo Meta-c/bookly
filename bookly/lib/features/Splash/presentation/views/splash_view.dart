3900ecb78234b595daae0a67abd2811dcec36212
Meta-c <metamedhat116@gmail.com>
2023-03-16 09:03:15 +0200

    splash view ui( no animation)

--- bookly/lib/features/Splash/presentation/views/splash_view.dart
+++ bookly/lib/features/Splash/presentation/views/splash_view.dart
@@ -10,7 +10,7 @@ class SplashView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    return const Scaffold(
       body: SplashViewBody(),
     );
   }
